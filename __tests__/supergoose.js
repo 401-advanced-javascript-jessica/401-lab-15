5ffef6a93bc7861e61f68373ca60596f82065a03
jessicakwalters <jessicakwalters@gmail.com>
2019-09-03 17:14:56 -0700

    fix linter error

--- __tests__/supergoose.js
+++ __tests__/supergoose.js
@@ -24,7 +24,7 @@ supergoose.startDB = async () => {
 
   const mongooseOptions = {
     useNewUrlParser:true,
-    useCreateIndex: true
+    useCreateIndex: true,
   };
 
   await mongoose.connect(mongoUri, mongooseOptions, (err) => {
